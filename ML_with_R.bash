4518b66fc008dbc3c5c5d6723733c04dc41deccf
AndrasB <66111581+balazsan@users.noreply.github.com>
2020-06-08 16:40:32 +0300

    Shell script added

---
+++ ML_with_R.bash
@@ -0,0 +1,29 @@
+## Script that downloads
+## some Jupyter notebooks 
+
+cd /home/balazsan
+
+# git reflog requires a name and email if user is not in passwd
+# even if you're only cloning
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+
+git clone https://github.com/balazsan/ML_with_R.git
+
+# clean directory
+rmdir work
+#rmdir keras-R-bash.bash
+
+# install tensorflow
+pip --no-cache-dir install tensorflow
+
+# install keras
+pip --no-cache-dir install PyYAML seaborn keras
+
+# fix for R to find tar, needed for github install
+export TAR=/bin/tar
+
+# install keras R-package
+R -e 'install.packages("vioplot", repo="http://cran.rstudio.com/")'             # check that installation works
+R -e 'install.packages("devtools", repo="http://cran.rstudio.com/")'            # get new version
+R -e 'devtools::install_github("rstudio/keras", upgrade = "always")'            # get lastest keras
