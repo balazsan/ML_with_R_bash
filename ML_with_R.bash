6b3533614c908236e2bdb786fd03a93b21d7f330
AndrasB <andras.balazs@luke.fi>
2020-06-29 10:34:13 +0300

    Update ML_with_R.bash
    New template with pre-installed Kear and TF added by CSC, no need to install these via start-up script

--- ML_with_R.bash
+++ ML_with_R.bash
@@ -13,16 +13,19 @@ git clone https://github.com/balazsan/ML_with_R.git
 # clean directory
 rmdir work
 
+# image named 'Jupyter R with Keras - 3 GB RAM'
+# was added to CSC's templates, no need to install anything
+
 # install tensorflow
-pip --no-cache-dir install tensorflow
+#pip --no-cache-dir install tensorflow
 
 # install keras
-pip --no-cache-dir install PyYAML seaborn keras
+#pip --no-cache-dir install PyYAML seaborn keras
 
 # fix for R to find tar, needed for github install
 export TAR=/bin/tar
 
 # install keras R-package
-R -e 'install.packages("vioplot", repo="http://cran.rstudio.com/")'             # check that installation works
-R -e 'install.packages("devtools", repo="http://cran.rstudio.com/")'            # get new version
-R -e 'devtools::install_github("rstudio/keras", upgrade = "always")'            # get lastest keras
+#R -e 'install.packages("vioplot", repo="http://cran.rstudio.com/")'             # check that installation works
+#R -e 'install.packages("devtools", repo="http://cran.rstudio.com/")'            # get new version
+#R -e 'devtools::install_github("rstudio/keras", upgrade = "always")'            # get lastest keras
